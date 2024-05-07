a6125a64b59a313bc592d14ca1512101ece04bc2
David-Asafe <davidasafe07@gmail.com>
2024-05-07 15:57:34 -0400

    atividade comparação-de-valor-e-tipo

---
+++ Operadores/comparacao-de-valor-e-tipo.js
@@ -0,0 +1,5 @@
+var a = 10; // Variável do tipo Number
+var b = "10"; // Variável do tipo String
+var c = b === a; // Comparação de valor e de tipo
+console.log(c); // Saída: False
+//Qual é o resultado?
