7cab8604c43b1d5532a1fed525ddef704ea16a25
David-Asafe <davidasafe07@gmail.com>
2024-05-07 17:15:54 -0400

    atividade operador-ternario-04

---
+++ Operadores/operador-ternario-04.js
@@ -0,0 +1,4 @@
+const firstCheck = false;
+const secondCheck = false;
+const access = firstCheck ? "Acesso Negado" : secondCheck ? "AcessoNegado" : "Acesso Concedido";
+console.log(access); // Saída: "Acesso concedido"
