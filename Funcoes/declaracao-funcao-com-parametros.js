00e29eb4b00ac6725f1acad08cf4ea868566c0f3
David-Asafe <davidasafe07@gmail.com>
2024-05-14 13:51:04 -0400

    atividade declaracao funcao com pareamento java

---
+++ Funcoes/declaracao-funcao-com-parametros.js
@@ -0,0 +1,7 @@
+function nomeDaFuncao(parametro1, parametro2) {
+    // corpo da função
+    // Pode conter uma ou mais intruções
+    return resultado; // Opcional
+}
+ // Chamada de função
+ nomeDaFuncao(parametro1, parametro2);
