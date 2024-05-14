b846f729295557cfdc2dd3d40940918b3b5646c7
David-Asafe <davidasafe07@gmail.com>
2024-05-14 13:38:05 -0400

    atividade if-else java(09/05)

---
+++ EstruturasCondicionais/if-else.js
@@ -0,0 +1,7 @@
+let diaDaSemana = "Quarta-feira";
+
+if (diaDaSemana === "sábado" || diaDaSemana === "domingo") {
+    console.log("É o fim de semana!");
+} else {
+    console.log("É dia de semana.");
+}
